0fdf23da29e8a8329736994c1d684acc1ef763be
Mern <mar-vis10@hotmail.com>
2019-05-10 10:57:06 +0200

    for chef

--- Lucky7/Assets/Scripts/MapHandler.cs
+++ Lucky7/Assets/Scripts/MapHandler.cs
@@ -6,7 +6,11 @@ using UnityEngine.Events;
 public class MapHandler : MonoBehaviour
 {
     [SerializeField] private int Width, Length;
-    [SerializeField] MapItem roadPreFab;
+    [SerializeField] MapItem roadStraight, roadCorner, roadTshape, roadCrossRoad;
+    enum RoadOrientation {  LeftToRight = 0, UpToDown = 90,
+                            TopFacing = 0, RightFacing = 90, DownFacing = 180, LeftFacing = 270,
+                            UpperLeftCorner = 0, UpperRightCorner = 90, LowerRightCorner = 180, LowerLeftCorner = 270,
+                            CrossRoad = 0 };
     [SerializeField] List<MapItem> housePrefabs;
     [SerializeField] MapItem truckDepotPrefab;
     [SerializeField] List<Trash> ThrashPrefabs;
@@ -33,7 +37,8 @@ public class MapHandler : MonoBehaviour
         //unless defined otherwise, we now have an 10 by 10 map.
         //it still has to be generated though.
 
-        LoadPreMadeMap(); //this will instantiate a premade map.
+        //LoadTestingMap(); //this will instantiate a premade testing map.
+        LoadTestingMap(); //this will instantiate a premade playing map.
     }
 
     // Update is called once per frame
@@ -45,7 +50,7 @@ public class MapHandler : MonoBehaviour
     /*  This function creates a simple map for testing.
      *  the map is 9x9 and not random.
      */
-    void LoadPreMadeMap()
+    void LoadTestingMap()
     {
         Width = 9;
         Length = 9;
@@ -87,6 +92,21 @@ public class MapHandler : MonoBehaviour
         PlaceBuilding(1, 7);
         PlaceBuilding(7, 1);
         PlaceBuilding(7, 7);
+    }
+
+
+
+    /*  this will load a 21x21 map. 
+     *  this map is meant for playing.
+     *  this map is not random.
+     */
+    void LoadGameMap()
+    {
+        GameMap = new MapItem[21, 21]; //generate an empty 21x21 sized map
+
+        GameMap[10, 10] = Instantiate(truckDepotPrefab, new Vector3(10, 0, 10), Quaternion.identity); //place the depot center piece
+        PlaceRoad(0, 0, roadCorner, RoadOrientation.UpperLeftCorner);
+
     }
 
     /*  This will place a building on GameMap[x,y]
@@ -134,9 +154,16 @@ public class MapHandler : MonoBehaviour
         return null;
     }
 
+    void PlaceRoad( int x, int y, MapItem roadType, RoadOrientation orientation )
+    {
+        MapItem tmp = Instantiate(roadType, new Vector3(x, 0, y), new Quaternion(0, (int)orientation, 0, 0));
+        tmp.GetComponent<Road>().trashPrefabs = ThrashPrefabs;
+        GameMap[x, y] = tmp;
+    }
+
     void PlaceRoad( int x, int y )
     {
-        MapItem tmp = Instantiate(roadPreFab, new Vector3(x, 0, y), Quaternion.identity);
+        MapItem tmp = Instantiate(roadStraight, new Vector3(x, 0, y), Quaternion.identity );
         tmp.GetComponent<Road>().trashPrefabs = ThrashPrefabs;
         GameMap[x, y] = tmp;
     }
